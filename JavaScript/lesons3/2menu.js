8c142264f90617aa40ec9425f56909ec7554647c
Sergorsha <alo109@mail.ru>
2021-02-16 14:10:04 +0300

    domashka

---
+++ JavaScript/lesons3/2menu.js
@@ -0,0 +1,10 @@
+"use srict"
+
+let a = 17;
+let b = 10;
+alert(a-b);
+let c = (a-b);
+let d = 7;
+alert(c+d);
+let result = (c+d);
+alert(result)
---
+++ JavaScript/lesons3/menu.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let num = 1;
+num =  + 12;      
+num =  - 14;
+num =  * 5;
+num =  / 7;
+num = 1 +;
+num = 1 -;
+alert(num);
